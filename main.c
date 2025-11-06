838bef5f5fa54237e668c4927247c7130e6cb6b4
yerimlee <144992260+limye2@users.noreply.github.com>
2025-11-06 15:51:22 +0900

    실습5

--- main.c
+++ main.c
@@ -7,13 +7,27 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    char src[] = "The worst things to eat before you sleep";
-    char dst[100];
+    FILE* fp;
+    char input[100];
+    int i;
     
-    strcpy(dst, src);
+    //1.open file //경로 직접 입력 option+command
+    fp = fopen("/Users/iyelim/10/sample.txt", "w");
     
-    printf("copied string : %s\n", dst);
+    //2.write file
+    for(i=0; i<3; i++)
+    {
+        //2-1. print "input a word"
+        printf("input a word:");
+        //2-2. scanf a string
+        scanf("%s", input);
+        //2-3. fprintf()
+        fprintf(fp, "%s\n", input);
+    }
+    //3. close filr
+    fclose(fp);
     return 0;
 }
